ee8cf352b07b18ef70347865b6330f4d3d9483b7
Mihai Moisa <mihai.moisa@ft.com>
2017-10-02 16:53:20 +0300

    Removed use of StringUtils

--- src/main/java/com/ft/methodearticleinternalcomponentsmapper/health/RemoteServiceHealthCheck.java
+++ src/main/java/com/ft/methodearticleinternalcomponentsmapper/health/RemoteServiceHealthCheck.java
@@ -7,7 +7,6 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 
 import com.sun.jersey.api.client.WebResource;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -57,7 +56,7 @@ public class RemoteServiceHealthCheck extends AdvancedHealthCheck {
         ClientResponse response = null;
         try {
             WebResource.Builder builder = client.resource(remoteServiceUri).getRequestBuilder();
-            if (StringUtils.isNotBlank(hostHeader)) {
+            if (hostHeader != null && !hostHeader.isEmpty()) {
                 builder.header("Host", hostHeader);
             }
             response = builder.get(ClientResponse.class);
